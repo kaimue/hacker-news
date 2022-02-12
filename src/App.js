f89a733a4ed476f8ebb21bf485a82b2d7023e597
kaimue <92952727+kaimue@users.noreply.github.com>
2022-02-12 15:26:45 +0100

    Worked on Search Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+// import Form from "./components/Form/Form";
+// import List from "./components/List/List";
 
 function App() {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [wordQuery, setWordQuery] = useState("React");
+
+  const url = "http://hn.algolia.com/api/v1/";
+  const queryParams = "search?query=";
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const wordQuery = "React";
+      // const wordQuery = inputField.value
+      // const wordQuery = event => event.target.value
+      const endpoint = `${url}${queryParams}${wordQuery}`;
+      try {
+        setLoading(true);
+        const res = await fetch(endpoint, { cache: "no-cache" });
+        if (res.ok) {
+          const data = await res.json();
+          console.log("Data", data);
+          setNews(data);
+          setLoading(false);
+        }
+        console.error("Fetch error!");
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    console.log("News", news);
+
+    fetchNews();
+  }, [wordQuery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hacker News</h1>
+
+      <div>
+        <label>Search:</label>
+        <input
+          onChange={() => setWordQuery}
+          type="text"
+          placeholder="Search Hacker News ..."
+        />
+      </div>
+
+      <div>
+        {(loading && <p>Loading ...</p>) || (
+          <ul>
+            {/* {news.map((item) => (
+              <li key={item.id}>{item.title}</li>
+            ))} */}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form>
+        <label>Search:</label>
+        <input type="text" placeholder="Search Hacker News ..."></input>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
