bfa08e493b555091f367a3927ec69e0a8e6213f5
kaimue <92952727+kaimue@users.noreply.github.com>
2022-02-26 14:51:26 +0100

    added pagination and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
   const [wordQuery, setWordQuery] = useState("React");
 
+  const [currPage, setCurrPage] = useState(1);
+
   const url = "http://hn.algolia.com/api/v1/";
   const queryParams = "search?query=";
+
+  // const navigate = useNavigate();
 
   const List = () => {
     if (loading) {
       return <p>Loading ...</p>;
-    } else if (news.length == 0) {
+    } else if (news.length === 0) {
       return <p>No Results Found ...</p>;
     } else {
+      console.log(news);
       return (
-        <ul>
-          {news.map((item) => (
-            <li key={item.objectID}>{item.title}</li>
-          ))}
-        </ul>
+        <>
+          <ul class="list-group">
+            {currItems.map((item) => (
+              <li
+                class="list-group-item list-group-item-action"
+                key={item.objectID}
+              >
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+          <br></br>
+          <ul class="pagination">
+            <li class="page-item">
+              {/* <a class="page-link" onClick={() => navigate(-1)}>
+                Previous
+              </a> */}
+              <a class="page-link" href={`page-${1}`}>
+                Previous
+              </a>
+            </li>
+            {pageNumbers.map((number) => (
+              <li class="page-item" key={number}>
+                <a
+                  class="page-link"
+                  onClick={() => setCurrPage(number)}
+                  href={`page-${number}`}
+                >
+                  {number}
+                </a>
+              </li>
+            ))}
+            <li class="page-item">
+              <a class="page-link">Next</a>
+            </li>
+          </ul>
+        </>
       );
     }
   };
@@ -34,9 +72,11 @@ function App() {
         if (res.ok) {
           const data = await res.json();
           setNews(data.hits);
+          console.log();
           setLoading(false);
         } else {
           console.error("Fetch error!");
+          alert("Sehr Schlimme Sache!");
         }
       } catch (e) {
         console.log(e.message);
@@ -46,18 +86,31 @@ function App() {
     fetchNews();
   }, [wordQuery]);
 
-  return (
-    <div>
-      <h1>Hacker News</h1>
+  const lastItem = currPage * 5;
+  const firstItem = lastItem - 5;
+  const currItems = news.slice(firstItem, lastItem);
 
-      <div>
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Number(news.length / 5); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div class="container">
+      <br></br>
+      <h1>Hacker News</h1>
+      <br></br>
+      <div class="form-group">
         <label>Search:</label>
         <input
+          class="form-control"
           onChange={(event) => setWordQuery(event.target.value)}
           type="text"
           placeholder="Search Hacker News ..."
         />
       </div>
+      <br></br>
       <div>{List()}</div>
     </div>
   );
